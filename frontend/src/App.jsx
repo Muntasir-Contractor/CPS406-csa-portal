5b2e13cd47ff188d1a1b50cb09b440a636d6677a
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-02-27 14:47:11 -0500

    Nav bar setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,26 +8,20 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="Navbar">
+        <ul className="nav_links">
+          <li>
+            <a href="#">Student Login</a>
+          </li>
+          <li>
+            <a href="#">Coordinator Login</a>
+          </li>
+          <li>
+            <a href="#"> Employer Login</a>
+          </li>
+        </ul>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
